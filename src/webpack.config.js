37d1a2d1287bea374461ce63fcaa09fbe041b133
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-10-01 11:26:06 -0400

    Update webpack.config.js
    Signed-off-by: Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,56 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
- 
-const TerserPlugin = require('terser-webpack-plugin');
-
-class AddLicenseAfterTerserPlugin {
-    constructor(options) {
-        this.options = options;
-    }
-
-    apply(compiler) {
-        compiler.hooks.afterEmit.tap('AddLicenseAfterTerserPlugin', compilation => {
-            const outputPath = this.options.outputPath || compiler.options.output.path;
-            const outputFileName = this.options.outputFileName || compiler.options.output.filename;
-
-            // Construct the full path to the output file
-            const outputFilePath = path.join(outputPath, outputFileName);
-
-            // Read the existing file content
-            fs.readFile(outputFilePath, 'utf8', (err, data) => {
-                if (err) throw err;
-
-                // Add your license text after minification (Terser)
-                const licenseText = `${packageData.LICENSE} `;
-
-                // Append license text to the existing file content
-                const newContent = licenseText + data;
-
-                // Write back the modified content to the output file
-                fs.writeFile(outputFilePath, newContent, 'utf8', err => {
-                    if (err) throw err;
-                    console.log(`License added to ${outputFileName}`);
-                });
-            });
-        });
-    }
-}
-
-// taken from https://github.com/webpack/webpack/issues/12506#issuecomment-1360810560
-class RemoveLicenseFilePlugin {
-    apply(compiler) {
-        compiler.hooks.emit.tap("RemoveLicenseFilePlugin", (compilation) => {
-            // compliation has assets to output
-            // console.log(compilation.assets);
-            for (let name in compilation.assets) {
-                if (name.endsWith("LICENSE.txt")) {
-                    delete compilation.assets[name];
-                }
-            }
-        });
-    }
-}
+  
 
 
 module.exports = {
@@ -66,11 +17,7 @@ module.exports = {
   experiments: {
     outputModule: true,
   },
-  optimization: {
-    minimizer: [new TerserPlugin({
-      extractComments: false,
-    })],
-  },
+  optimization: { },
   plugins: [],
   module: {
     rules: [
@@ -87,6 +34,6 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.ts'],
+    extensions: ['.ts', '.js'],
   },
 };
